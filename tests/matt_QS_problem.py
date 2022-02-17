4e7f2e26375359cc483034a30d89674dd622379f
Misha Padidar <map454@cornell.edu>
2022-02-17 13:10:05 -0500

    udpates to matts qs problem

--- tests/matt_QS_problem.py
+++ tests/matt_QS_problem.py
@@ -6,8 +6,7 @@ from simsopt.mhd.vmec_diagnostics import QuasisymmetryRatioResidual
 from simsopt.objectives.graph_least_squares import LeastSquaresProblem
 from simsopt.solve.graph_mpi import least_squares_mpi_solve
 
-# 1 group
-mpi = MpiPartition(1)
+mpi = MpiPartition(7)
 
 vmec = Vmec("../problem/input.nfp4_QH_warm_start", mpi=mpi)
 
@@ -19,6 +18,7 @@ surf.fixed_range(mmin=0, mmax=max_mode,
                  nmin=-max_mode, nmax=max_mode, fixed=False)
 surf.fix("rc(0,0)") # Major radius
 
+print("\n\n\n\n")
 print('Parameter space:', surf.dof_names)
 
 # Configure quasisymmetry objective:
@@ -30,13 +30,17 @@ qs = QuasisymmetryRatioResidual(vmec,
 prob = LeastSquaresProblem.from_tuples([(vmec.aspect, 7, 1),
                                         (qs.residuals, 0, 1)])
 
+print("\n\n\n\n")
 print("Quasisymmetry objective before optimization:", qs.total())
 print("Total objective before optimization:", prob.objective())
 
 import time
 t0 = time.time()
 
+print("\n\n\n\n")
 least_squares_mpi_solve(prob, mpi, grad=True)
 
 print("\n\n\n\n")
 print('Time to solve: ',time.time() - t0)
+print('Total Obj: ',prob.objective())
+print('Total QS: ',qs.total())
