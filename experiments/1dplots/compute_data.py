0e354c7b7097a30c8b61e831db8d4fe607e16c8c
Misha Padidar <map454@cornell.edu>
2022-02-20 17:59:30 -0500

    1d plot now uses evalp()

--- experiments/1dplots/compute_data.py
+++ experiments/1dplots/compute_data.py
@@ -3,14 +3,16 @@ import sys
 sys.path.append("../../problem")
 import qh_prob1
 import pickle
+import sys
+
 
 # load the problem
 vmec_input = "../../problem/input.nfp4_QH_warm_start"
-n_partitions = 2
-prob = qh_prob1.QHProb1(vmec_input,n_partitions)
+prob = qh_prob1.QHProb1(vmec_input)
 x0 = prob.x0
 dim_x = prob.dim_x
 dim_F = prob.dim_F
+np.random.seed(0) # match the seeds
 
 # discretization parameters
 n_directions = dim_x
@@ -20,9 +22,11 @@ n_points_per = 200 # total points per direction
 max_pert = 0.5
 ub = max_pert
 lb = -max_pert
-T1 = np.linspace(lb,ub,int(n_points_per/2))
-min_log,max_log = -4,-1
-T2 = np.logspace(min_log,max_log,int(n_points_per/4))
+n1 = int(n_points_per/2)
+T1 = np.linspace(lb,ub,n1)
+min_log,max_log = -8,-3
+n2 = int((n_points_per - n1)/2)
+T2 = np.logspace(min_log,max_log,n2)
 T2 = np.hstack((-T2,T2))
 T = np.sort(np.unique(np.hstack((T1,T2))))
 
@@ -35,13 +39,14 @@ X = np.zeros((n_directions,n_points_per,dim_x))
 FX = np.zeros((n_directions,n_points_per,dim_F))
 
 for ii in range(n_directions):
-  for jj,tt in enumerate(T):
+    print(f"direction {ii}/{dim_x}")
+    sys.stdout.flush()
     # eval point
-    xx = np.copy(x0 + tt*Q[ii])
-    fx = prob.eval(xx)
+    Y = x0 + Q[ii]*np.reshape(T,(-1,1))
+    fY = prob.evalp(Y)
     # save it
-    X[ii,jj] = np.copy(xx)
-    FX[ii,jj] = np.copy(fx)
+    X[ii] = np.copy(Y)
+    FX[ii] = np.copy(fY)
 
 # dump a pickle file
 outfile = "1dplot_data.pickle"
