41472761c77f5b627dd470f42198a09f43de10d6
Misha Padidar <map454@cornell.edu>
2022-02-24 17:32:28 -0500

    wrote a parallel safe evaluation function

--- utils/safe_eval.py
+++ utils/safe_eval.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import pickle
 import subprocess
+from multiprocessing import Pool
 
 def safe_eval(yy,dim_F,vmec_input):
   """
@@ -26,8 +27,55 @@ def safe_eval(yy,dim_F,vmec_input):
   indata = pickle.load(open('safe_eval.pickle','rb'))
   return indata['F']
   
+# define a function for pool
+def subpcall(ii):
+  # subprocess call
+  bashCommand = f"mpiexec -n 1 python3 safe_eval.py _safe_eval_{ii}.pickle"
+  process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+  output, error = process.communicate()
+
+def safe_evalp(Y,dim_F,vmec_input,n_partitions):
+  """
+  Do a parallel evaluation safely.
+  Y: 2D array of points
+  dim_F: dimension of function output, expected >1
+  vmec_input: vmec input file
+  n_partitions: number of mpi ranks to use for evals.
+
+  Notes:
+  The driver script that calls this function does not need to be run with MPI, 
+  i.e. just do python3 driver_script.py. However,
+  Make sure that slurm allocates at least n_partitions mpi ranks for the job.
+  To do this include the following in your slurm .submission file 
+    #SBATCH -N n_partitions
+    #SBATCH -n n_partitions
+  """
+  n_points = len(Y)
+  # write pickle files
+  for ii in range(n_points):
+    outdata = {}
+    outdata['x'] = Y[ii]
+    outdata['F'] = np.inf*np.ones(dim_F)
+    outdata['vmec_input'] = vmec_input
+    pickle.dump(outdata,open(f'_safe_eval_{ii}.pickle','wb'))
+  
+  # do the evals with pool
+  with Pool(n_partitions) as p:
+    p.map(subpcall, range(n_points))
+
+  # storage
+  FY = np.zeros((n_points,dim_F))
+
+  # read the pickle files
+  for ii in range(n_points):
+    indata = pickle.load(open(f'_safe_eval_{ii}.pickle','rb'))
+    FY[ii] = np.copy(indata['F'])
+  return FY
   
 if __name__=="__main__":  
+  """
+  Here is the actual function evaluation that is performed.
+  """ 
   import sys
   import pickle
   sys.path.append("../problem")
