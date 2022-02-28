ef260641106123fda13e7fee6b4c91d0fa62a41a
Misha Padidar <map454@cornell.edu>
2022-02-28 16:05:11 -0500

    now computes bounds for F as well as x

--- experiments/data_based_constraints/combine_bounds.py
+++ experiments/data_based_constraints/combine_bounds.py
@@ -18,16 +18,26 @@ def combine_bounds_from_files(filelist):
     if ii == 0:
       lb = indata['lb']
       ub = indata['ub']
+      #F_lb = np.min(indata['FX'],axis=0)
+      F_ub = np.max(indata['FX'],axis=0)
     else:
       lb_f = indata['lb']
       ub_f = indata['ub']
       lb = np.copy(np.minimum(lb,lb_f))
       ub = np.copy(np.maximum(ub,ub_f))
-  return lb,ub
+
+      #lb_temp = np.min(indata['FX'],axis=0)
+      ub_temp = np.max(indata['FX'],axis=0)
+      #F_lb = np.copy(np.minimum(lb_temp,F_lb)) # analytic lower bound
+      F_lb = np.zeros_like(F_ub)
+      F_ub = np.copy(np.maximum(ub_temp,F_ub))
+     
+  return lb,ub,F_lb,F_ub
 
 if __name__=="__main__":
   import glob
   filelist = glob.glob("./data/samples_*.pickle")
-  lb,ub = combine_bounds_from_files(filelist)
-  d = {'lb':lb,'ub':ub}
+  lb,ub,F_lb,F_ub = combine_bounds_from_files(filelist)
+  d = {'lb':lb,'ub':ub,'F_lb':F_lb,'F_ub':F_ub}
+  print(d)
   pickle.dump(d, open("../../problem/bounds.nfp4_QH_warm_start_high_res.pickle","wb"))
