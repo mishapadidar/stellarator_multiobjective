7b0db89c79ebbcf5660b282f28c0870ace0282bc
Misha Padidar <map454@cornell.edu>
2022-02-28 14:28:49 -0500

    modified to handle vector valued functions

--- utils/eval_wrapper.py
+++ utils/eval_wrapper.py
@@ -5,27 +5,30 @@ class eval_wrapper:
   """
   Class for wrapping a function call so that we can 
   save evaluations.
-  f: function handle
-  dim: function input dimension
+  F: function handle
+  dim_x: function input dimension
+  dim_F: function output dimension
   Usage:
-    f = lambda x: np.linalg.norm(x)
-    dim = 2
-    func = eval_wrapper(f,dim)
+    F = lambda x: [np.linalg.norm(x),np.sum(x)]
+    dim_x = 3
+    dim_F = 2
+    func = eval_wrapper(f,dim_x,dim_F)
     # call the function
-    func(np.random.randn(dim))
+    func(np.random.randn(dim_x))
     # check the history
     print(func.X)
-    print(func.fX)
+    print(func.FX)
   """
 
-  def __init__(self,f,dim):
-    self.dim = dim
-    self.X = np.zeros((0,dim))
-    self.fX = np.zeros(0)
-    self.func = f
+  def __init__(self,F,dim_x,dim_F):
+    self.dim_x = dim_x
+    self.dim_F = dim_F
+    self.X = np.zeros((0,dim_x))
+    self.FX = np.zeros((0,dim_F))
+    self.func = F
 
   def __call__(self,xx):
     ff = self.func(xx)
     self.X = np.append(self.X,[xx],axis=0)
-    self.fX = np.append(self.fX,ff)
+    self.FX = np.append(self.FX,[ff],axis=0)
     return ff
