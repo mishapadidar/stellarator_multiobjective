caf04dc115ca806a6cad319aef883c0fd5521902
Misha Padidar <map454@cornell.edu>
2022-03-04 11:15:41 -0500

    created a rawp method for parallel raw evals over the partitions

--- problem/qh_prob1.py
+++ problem/qh_prob1.py
@@ -91,6 +91,48 @@ class QHProb1():
     obj[np.isnan(obj)] = np.inf
     return obj
 
+  def rawp(self,Y):
+    """
+    Evaluate the raw objective vector at many points by distributing
+    the resources over worker groups.
+
+    This function expects all workers groups to contribute, so 
+    all worker groups must evaluate this function on the same
+    set of points Y.
+
+    Y: array of input variables describing surface
+    Y: array of size (n_points, self.dim_x).
+
+    return: array of objectives [QS, aspect]
+    return: array of size (n_points, self.dim_F)
+    """
+    n_points = np.shape(Y)[0]
+
+    # special case for 1 partition
+    if self.n_partitions ==1:
+      return np.array([self.raw(y) for y in Y])
+
+    # divide the evals across groups
+    idxs,counts = divide_work(n_points,self.n_partitions)
+    idx = idxs[self.mpi.group]
+
+    # do the evals
+    f   = np.array([self.raw(y) for y in Y[idx]]).flatten()
+
+    # head leader gathers all evals
+    F = np.zeros(n_points*self.dim_F)
+    counts = self.dim_F*np.array(counts).astype(int)
+    self.mpi.comm_leaders.Gatherv(f,(F,counts),root=0)
+    # broadcast to leaders
+    self.mpi.comm_leaders.Bcast(F,root=0)
+    # broadcast internally within group
+    self.mpi.comm_groups.Bcast(F,root=0)
+
+    # reshape to 2D-array
+    F = np.reshape(F,(-1,self.dim_F))
+    
+    return np.copy(F)
+
   def eval(self,y,raw=[]):
     """
     Evaluate the objective vector.
@@ -112,7 +154,7 @@ class QHProb1():
     obj[np.isnan(obj)] = np.inf
     return obj
 
-  def evalp(self,Y):
+  def evalp(self,Y,Raw=[]):
     """
     Evaluate the objective vector at many points by distributing
     the resources over worker groups.
@@ -122,37 +164,20 @@ class QHProb1():
     set of points Y.
 
     Y: array of input variables describing surface
-    Y: array of size (n_points, self.dim_x). n_points must be a 
-       multiple of n_partitions.
+    Y: array of size (n_points, self.dim_x).     
+    Raw: (optional), array of precomputed raw simulation output
+    Raw: array of size (n_points, self.dim_F).
 
     return: array of objectives [QS objective, aspect objective]
     return: array of size (n_points, self.dim_F)
     """
     n_points = np.shape(Y)[0]
 
-    # special case for 1 partition
-    if self.n_partitions ==1:
-      return np.array([self.eval(y) for y in Y])
-
-    # divide the evals across groups
-    idxs,counts = divide_work(n_points,self.n_partitions)
-    idx = idxs[self.mpi.group]
-
-    # do the evals
-    f   = np.array([self.eval(y) for y in Y[idx]]).flatten()
-
-    # head leader gathers all evals
-    F = np.zeros(n_points*self.dim_F)
-    counts = self.dim_F*np.array(counts).astype(int)
-    self.mpi.comm_leaders.Gatherv(f,(F,counts),root=0)
-    # broadcast to leaders
-    self.mpi.comm_leaders.Bcast(F,root=0)
-    # broadcast internally within group
-    self.mpi.comm_groups.Bcast(F,root=0)
-
-    # reshape to 2D-array
-    F = np.reshape(F,(-1,self.dim_F))
-    
+    # do the evals in parallel
+    if Raw == []:
+      Raw = self.rawp(Y)
+    # turn the raw into objectives
+    F   = np.array([self.eval(Y[ii],Raw[ii]) for ii in range(n_points)])
     return np.copy(F)
 
   def jac(self,y,h=1e-7,method='forward'):
@@ -227,7 +252,7 @@ if __name__=="__main__":
 
   # test 1:
   # evaluate obj and jac with one partition
-  test_1 = True
+  test_1 = False
   if test_1 == True:
     prob = QHProb1(n_partitions=1,max_mode=2)
     x0 = prob.x0
@@ -244,14 +269,17 @@ if __name__=="__main__":
   
   # test 2:
   # evaluate obj and jac with multiple partition
-  test_2 = False
+  test_2 = True
   if test_2 == True:
-    prob = QHProb1()
+    prob = QHProb1(max_mode=1)
     x0 = prob.x0
     n_evals = 10
     Y = x0 + 1e-5*np.random.randn(n_evals,prob.dim_x)
     import time
     t0 = time.time()
+    Raw = prob.rawp(Y)
+    print(Raw)
+    print(prob.evalp(Y,Raw))
     print(prob.evalp(Y))
     print("\n\n\n")
     print('eval time',time.time() - t0)
