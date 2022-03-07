c2bc7bcc9499e2f2b0f20e089ddfce8983fde939
Misha Padidar <map454@cornell.edu>
2022-03-07 09:52:29 -0500

    updated with new objective

--- experiments/qh_prob1_moo/mo_opt_nsga2.py
+++ experiments/qh_prob1_moo/mo_opt_nsga2.py
@@ -4,47 +4,58 @@ from platypus import NSGAII,Problem,Real
 import sys
 sys.path.append("../../utils")
 sys.path.append("../../problem")
+from rescale import *
 import safe_eval
 from eval_wrapper import eval_wrapper
 from is_pareto_efficient import is_pareto_efficient
 
-# starting point
-x0_input = "../../problem/x0.nfp4_QH_warm_start_high_res.pickle"
-x0 = pickle.load(open(x0_input,"rb"))
-dim_x = len(x0)
+
+# load the bound constraints
+bounds = pickle.load(open("../../problem/bounds.nfp4_QH_warm_start_high_res.pickle","rb"))
+x_lb,x_ub,F_lb,F_ub = bounds['lb'],bounds['ub'],bounds['F_lb'],bounds['F_ub']
+
+# set F_lb to the analytic lower bound
+F_lb = 0.0*F_lb
+
+# sizes
+dim_x = len(x_lb)
+dim_F = len(F_lb)
 
 # start up a safe evaluator
 vmec_input = "../../problem/input.nfp4_QH_warm_start_high_res"
 eval_script = "./qh_prob1_safe_eval.py"
-dim_F = 2
 evaluator = safe_eval.SafeEval(dim_F,vmec_input,eval_script)
 # wrap the evaluator
 func_wrap = eval_wrapper(evaluator.eval,dim_x,dim_F)
 
-#max_eval = 3000
 max_eval = int(sys.argv[1])
 
 # set the seed
 seed = np.random.randint(int(1e8))
 np.random.seed(seed)
 
-# load the bound constraints
-bounds = pickle.load(open("../../problem/bounds.nfp4_QH_warm_start_high_res.pickle","rb"))
-lb,ub,F_lb,F_ub = bounds['lb'],bounds['ub'],bounds['F_lb'],bounds['F_ub']
 
-# TODO: run without bounds... they may be over constraining
-lb,ub = -np.inf*np.ones(dim_x),np.inf*np.ones(dim_x)
+# TODO: Rescale the input variables
 
-# rescale the objectives
 def objective(xx):
-  return (func_wrap(xx) - F_lb)/(F_ub - F_lb)
+  """
+  Optimize a non-smooth rescaled objective.
+  The QS^2 upper bound is around 1e17 which would 
+  introduce a huge instability into our problem if we
+  rescaled by it.
+
+  Instead we optimize the non-smooth objectives 
+  [|QS|, |Aspect-A_target|]
+  and rescale these instead.
+  """
+  return to_unit_cube(func_wrap(xx),F_lb,F_ub)
 
 print("Running optimization")
 print(f"with {max_eval} evals")
 # set up NSGAII
 problem = Problem(dim_x, dim_F)
 for ii in range(dim_x):
-  problem.types[ii] = Real(lb[ii],ub[ii])
+  problem.types[ii] = Real(x_lb[ii],x_ub[ii])
 problem.function = objective
 algorithm = NSGAII(problem)
 algorithm.run(max_eval)
