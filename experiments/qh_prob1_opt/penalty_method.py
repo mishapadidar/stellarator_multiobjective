cd03f12e8161a5d7864b6b398cd2be544f0cb695
Misha Padidar <map454@cornell.edu>
2022-03-10 11:32:54 -0500

    using gradient descent for cheaper steps, made adaptive gtol and penalty parameter

--- experiments/qh_prob1_opt/penalty_method.py
+++ experiments/qh_prob1_opt/penalty_method.py
@@ -3,9 +3,11 @@ import pickle
 from scipy.optimize import minimize
 import sys
 sys.path.append("../../utils")
+sys.path.append("../../optim")
 sys.path.append("../../problem")
 import qh_prob1
 from eval_wrapper import eval_wrapper
+from gradient_descent import GD
 
 """
 Penalty Method to solve
@@ -28,32 +30,50 @@ if prob.mpi.proc0_world:
 else:
   master = False
 
+# pen parameter initialiization
+jac = prob.jacp(x0)
+pen_param = np.linalg.norm(jac[0])/np.linalg.norm(jac[1])
+pen_param*=10
+if master:
+  print('initial penalty parameter: ',pen_param)
+  print('initial |grad|: ',np.linalg.norm(jac[0] + pen_param*jac[1]))
+  print('initial |qs grad|: ',np.linalg.norm(jac[0]))
+  print('initial |aspect^2 grad|: ',np.linalg.norm(jac[1]))
+
+# set relative gtol
+gtol     = 1e-3*np.linalg.norm(jac[0] + pen_param*jac[1]) # stopping tolerance
+qs_gtol  = 1e-4*np.linalg.norm(jac[0]) # target gtol for qs
+
+if master:
+  print('gtol:',gtol)
+  print('qs gtol:',qs_gtol)
+
 max_eval = 3000 # evals per iteration
-gtol     = 1e-3 # stopping tolerance
-max_solves = 8 # number of penalty updates
-pen_param = 1.0 # penalty parameter initialization
+max_solves = 7 # number of penalty updates
 pen_inc = 10.0 # increase parameter
-ctol    = 1e-5 # target constraint tolerance
-qs_gtol = 1e-3 # target gtol for qs
+ctol    = 1e-4 # target constraint tolerance
 method  ='L-BFGS-B'
 options = {'maxfun':max_eval,'gtol':gtol}
 
 def con(xx):
   """ constraint """
-  ev = prob.eval(xx)
-  return ev[1]
+  prob.eval(xx)
+  ev = prob.vmec.aspect() - aspect_target
+  return ev
 # write the objective
 def obj(xx):
   """ penalty obj """
   ev  = prob.eval(xx)
   ret = ev[0] + pen_param*ev[1]
-  print(f'f(x): {ret}, qs err: {ev[0]}, con: {ev[1]}')
+  if master:
+    print(f'f(x): {ret}, qs err: {ev[0]}, aspect^2: {ev[1]}')
   return ret
 def grad(xx):
   """ penalty jac """
   jac = prob.jacp(xx)
   ret = jac[0] + pen_param*jac[1]
-  print('|grad|',np.linalg.norm(ret))
+  if master:
+    print('|grad|',np.linalg.norm(ret))
   return ret
 
 # wrap the objective
@@ -71,13 +91,14 @@ for ii in range(max_solves):
   if master:
     print("\n\n\n")
     print("iteration",ii)
-  res     = minimize(obj,x0,jac=grad,method=method,options=options)
-  xopt = res.x
+  #res     = minimize(obj,x0,jac=grad,method=method,options=options)
+  #xopt = res.x
+  xopt = GD(obj,grad,x0,alpha0 = 1e-1,gamma=0.5,max_iter=max_eval,gtol=gtol,c_1=1e-6,verbose=False)
   fopt = obj(xopt)
   copt = con(xopt)
   if master:
     print("\n\n\n")
-    print(res)
+    #print(res)
     print('optimal obj:',fopt)
     print('optimal con:',copt)
     sys.stdout.flush()
@@ -92,6 +113,8 @@ for ii in range(max_solves):
     grad_qs = prob.jacp(xopt)[0]
     if np.linalg.norm(grad_qs) <= qs_gtol:
       break
+    else:
+      gtol = qs_gtol/2
 
   # get run data
   X = func_wrap.X
@@ -104,6 +127,8 @@ for ii in range(max_solves):
     print(f"Dumping data to {outfilename}")
     outdata = {}
     outdata['xopt'] = xopt
+    outdata['fopt'] = fopt
+    outdata['copt'] = copt
     outdata['X'] = X
     outdata['FX'] = FX
     outdata['aspect_target'] = aspect_target
