c473a6e1a375cc1533dbee40c2b16da4a70d28e5
Misha Padidar <map454@cornell.edu>
2022-03-17 12:58:57 -0400

    small fixes

--- experiments/qh_prob1_opt/find_warm_start.py
+++ experiments/qh_prob1_opt/find_warm_start.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 import glob 
 
 def find_warm_start(aspect_target,dir_list,thresh =1e-4):
@@ -15,16 +16,24 @@ def find_warm_start(aspect_target,dir_list,thresh =1e-4):
   for ff in filelist:
     indata = pickle.load(open(ff,"rb"))
     xopt = indata['xopt']
+
+    # TODO: remove
+    X.append(xopt)
+    QX.append(indata['fopt'])
+    continue
+
     asp = indata['residuals'][-1] + indata['aspect_target']
     qs_mse = np.mean(indata['residuals'][:-1]**2)
     if abs(asp-asp_target)<=thresh:
       X.append(xopt)
-      Q.append(qs_mse)
+      QX.append(qs_mse)
 
   if len(QX) == 0:
     # restart
-    return find_warm_start(aspect_target,dir_list,10*thresh):
+    return find_warm_start(aspect_target,dir_list,10*thresh)
   else:
     # choose the best point within the threshold
-    return np.argmin(QX)
+    return X[np.argmin(QX)]
   
+if __name__=="__main__":
+  print(find_warm_start(5.2,["./data"]))
