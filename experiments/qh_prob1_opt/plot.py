f4a1e260c38294b07627207142f2b2fda3742c36
Misha Padidar <map454@cornell.edu>
2022-03-18 13:46:15 -0400

    updates to plot pareto curve

--- experiments/qh_prob1_opt/plot.py
+++ experiments/qh_prob1_opt/plot.py
@@ -5,6 +5,9 @@ from matplotlib.pyplot import cm
 import matplotlib
 matplotlib.use('TkAgg')
 import pickle
+import sys
+sys.path.append("../../utils")
+from is_pareto_efficient import is_pareto_efficient
 
 """
 plot QS^2 error via aspect
@@ -21,7 +24,16 @@ for ff in filelist:
   asp = RX[:,-1]
   aspect_list = np.append(aspect_list,asp)
   qs_list = np.append(qs_list,qs_mse)
-plt.scatter(aspect_list,qs_list,'-o',linewidth=3)
+
+# comppute pareto set
+FX = np.vstack((aspect_list,qs_list)).T
+idx_pareto = is_pareto_efficient(FX)
+idx_pareto = np.sort(idx_pareto)
+aspect_pareto = FX[idx_pareto,0]
+qs_pareto = FX[idx_pareto,1]
+
+plt.scatter(aspect_list,qs_list,'-o',alpha=0.5)
+plt.plot(aspect_pareto,qs_pareto,'-s',color='r',linewidth=3)
 plt.xlabel('aspect ratio')
 plt.ylabel('Quasisymmetry MSE')
 plt.yscale('log')
