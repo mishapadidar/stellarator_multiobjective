21c116f8eed5fd45e0df7db2f586060479858e7e
mishapadidar <map454@cornell.edu>
2022-03-29 22:26:28 -0400

    updated plot to use precomputed pareto set and, optionally, plot other optima from runs

--- experiments/qh_prob1_opt/plot.py
+++ experiments/qh_prob1_opt/plot.py
@@ -13,36 +13,28 @@ from is_pareto_efficient import is_pareto_efficient
 plot QS^2 error via aspect
 """
 
-filelist = glob.glob("./data/*.pickle")
-
-aspect_list = np.zeros(0)
-qs_list  = np.zeros(0)
-for ff in filelist:
-  print('loading',ff)
-  indata = pickle.load(open(ff,"rb"))
-  RX = indata['RawX']
-  qs_mse = np.mean(RX[:,:-1]**2,axis=1)
-  asp = RX[:,-1]
-  aspect_list = np.append(aspect_list,asp)
-  qs_list = np.append(qs_list,qs_mse)
+plot_all_optima = True
+if plot_all_optima:
+  filelist = glob.glob("./data/data*.pickle")
+  aspect_list = np.zeros(0)
+  qs_list  = np.zeros(0)
+  for ff in filelist:
+    print('loading',ff)
+    indata = pickle.load(open(ff,"rb"))
+    aspect_list = np.append(aspect_list,indata['aspect_opt'])
+    qs_list = np.append(qs_list,indata['qs_mse_opt'])
   FX = np.vstack((aspect_list,qs_list)).T
-  idx_pareto = is_pareto_efficient(FX)
-  aspect_list = aspect_list[idx_pareto]
-  qs_list = qs_list[idx_pareto]
 
-print('computing pareto set')
-# comppute pareto set
-FX = np.vstack((aspect_list,qs_list)).T
-idx_pareto = is_pareto_efficient(FX)
-aspect_pareto = FX[idx_pareto,0]
-qs_pareto = FX[idx_pareto,1]
-idx_sort = np.argsort(aspect_pareto)
-aspect_pareto = aspect_pareto[idx_sort]
-qs_pareto = qs_pareto[idx_sort]
+# load pareto set
+infilename = "./data/pareto_optimal_points.pickle"
+indata = pickle.load(open(infilename,"rb"))
+FX_pareto = indata['FX']
+aspect_pareto = np.copy(FX_pareto[:,0])
+qs_pareto = np.copy(FX_pareto[:,1])
 
-print('plotting')
+# plot
 plt.scatter(aspect_list,qs_list,alpha=0.5)
-plt.plot(aspect_pareto,qs_pareto,'-s',color='r',linewidth=3)
+plt.scatter(aspect_pareto,qs_pareto,s=20,color='r',label='pareto')
 plt.xlabel('aspect ratio')
 plt.ylabel('Quasisymmetry MSE')
 plt.yscale('log')
