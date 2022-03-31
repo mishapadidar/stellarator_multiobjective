127bba33649fa26ad376da43e3a73c2a92a51546
Misha Padidar <map454@cornell.edu>
2022-03-31 12:58:04 -0400

    set up some handling for the negative lagrange multiplier case

--- experiments/qh_prob1_opt/predictor_corrector.py
+++ experiments/qh_prob1_opt/predictor_corrector.py
@@ -320,9 +320,14 @@ def set_target(xx,lam_xx,aspect_step,qs_mse_xx,aspect_xx):
   qs_mse_xx: the qs_mse at xx
   aspect_xx: the aspect at xx
 
-  WARNING: positive lagrange multipliers will cause the target to 
+  WARNING: negative lagrange multipliers will cause the target to 
   be set in the opposite direction.
   """
+  if lam_xx < 0.0:
+    # protect against negative lagrange multipliers
+    qs_target_dec = 0.1*qs_mse_xx
+    target_k = np.array([aspect_xx,qs_target_dec])
+    return target_k
   qs_mse_step = - lam_xx*aspect_step
   tang_point = np.array([aspect_xx,qs_mse_xx]) + np.array([aspect_step,qs_mse_step])
   qs_mse_frac = 0.5 # fraction of qs_mse to decrease along normal step
