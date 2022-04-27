ed5fba08792b07a636ef8c2a60a7aaa9abeb279a
Misha Padidar <map454@cornell.edu>
2022-04-26 20:04:04 -0400

    adding more useful files

---
+++ experiments/qa_prob1_opt/find_pareto_front.py
@@ -0,0 +1,78 @@
+
+import numpy as np
+import os
+import pickle
+import glob 
+import sys
+sys.path.append("../../utils")
+from is_pareto_efficient import is_pareto_efficient
+
+def find_pareto_front(datadir,save=True,from_scratch=True):
+  """
+  Find the pareto front over a given set of files. 
+
+  datadir: directory holding the pickle files
+           should be formatted as i.e. "./data"
+  save: save the pareto front data
+  from_scratch: compute the pareto front from scratch. Use this
+   if you have overwritten any old files.
+  """
+
+  filelist = glob.glob(datadir +"/data*.pickle")
+  filelist.sort()
+
+  outfilename = datadir + "/pareto_optimal_points.pickle"
+  if os.path.exists(outfilename) and from_scratch is False:
+    indata = pickle.load(open(outfilename,"rb"))
+    X = indata['X']
+    FX = indata['FX']
+    aspect_list = np.copy(FX[:,0])
+    qs_list = np.copy(FX[:,1])
+    processed = indata['filelist']
+  else:
+    X = []
+    aspect_list = np.zeros(0)
+    qs_list  = np.zeros(0)
+    processed = []
+
+  for ff in filelist:
+
+    if ff in processed:
+      continue
+    else:
+      processed.append(ff)
+
+    # load data
+    print('loading',ff)
+    indata = pickle.load(open(ff,"rb"))
+    RX = indata['RawX']
+    qs_mse = np.mean(RX[:,:-1]**2,axis=1)
+    asp = RX[:,-1]
+
+    # append new data to lists
+    for xx in indata['X']:
+      X.append(xx)
+    aspect_list = np.append(aspect_list,asp)
+    qs_list = np.append(qs_list,qs_mse)
+
+    # compute pareto set
+    FX = np.vstack((aspect_list,qs_list)).T
+    idx_pareto = is_pareto_efficient(FX,return_mask=False)
+
+    # only keep pareto points
+    aspect_list = aspect_list[idx_pareto]
+    qs_list = qs_list[idx_pareto]
+    FX = FX[idx_pareto]
+    X  = [X[ii] for ii in idx_pareto]
+
+    if save:
+      outdata = {}
+      outdata['X'] = X
+      outdata['FX'] = FX
+      outdata['filelist'] = processed
+      pickle.dump(outdata,open(datadir + "/pareto_optimal_points.pickle","wb"))
+  return X,FX
+
+
+if __name__=="__main__":
+  find_pareto_front("./data",save=True,from_scratch=True)
---
+++ experiments/qa_prob1_opt/plot.py
@@ -0,0 +1,54 @@
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import matplotlib
+matplotlib.use('TkAgg')
+import pickle
+import sys
+sys.path.append("../../utils")
+from is_pareto_efficient import is_pareto_efficient
+
+import matplotlib
+plt.rc('font', family='serif')
+plt.rc('text.latex', preamble=r'\usepackage{amsmath,bm}')
+matplotlib.rcParams.update({'font.size': 16})
+
+"""
+plot QS^2 error via aspect
+"""
+
+plot_all_points = True
+
+# load data
+indata = pickle.load(open("./data/function_values.pickle","rb"))
+aspect_list = indata['aspect']
+qs_list = indata['qs_mse']
+# truncate plot to [3,10]
+idx_trunc = np.logical_and(aspect_list>=3,aspect_list<=10)
+aspect_list = aspect_list[idx_trunc]
+qs_list = qs_list[idx_trunc]
+# stack it
+FX = np.vstack((aspect_list,qs_list)).T
+# make pareto set
+idx_pareto = is_pareto_efficient(FX)
+aspect_pareto = FX[idx_pareto,0]
+qs_pareto = FX[idx_pareto,1]
+
+
+# plot
+plt.figure(figsize=(10,8))
+if plot_all_points:
+  plt.scatter(aspect_list,qs_list,alpha=0.5)
+plt.scatter(aspect_pareto,qs_pareto,s=20,color='r',label='pareto front')
+plt.xlabel('Aspect Ratio')
+plt.ylabel('Quasisymmetry MSE')
+plt.yscale('log')
+plt.legend()
+plt.tight_layout()
+plt.savefig(fname="./pareto_plot.png",format='png')
+plt.show()
+  
+
+
+
