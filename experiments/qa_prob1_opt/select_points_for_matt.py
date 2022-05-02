9f8c0f0ef3cb8f997fbe9c731c690d9143f55df6
Misha Padidar <map454@cornell.edu>
2022-05-02 14:08:29 -0400

    upgraded script to get more successful evaluations

--- experiments/qa_prob1_opt/select_points_for_matt.py
+++ experiments/qa_prob1_opt/select_points_for_matt.py
@@ -4,7 +4,9 @@ import pickle
 import sys
 sys.path.append("../../problem")
 sys.path.append("../../utils")
-import qh_prob1
+import qa_prob1
+import glob
+import os
 """
 Select pareto points for matt
 """
@@ -17,17 +19,42 @@ FX = indata['FX']
 aspect = np.copy(FX[:,0])
 qs = np.copy(FX[:,1])
 
-# set a target point aspect
-aspect_target =8.7
+max_mode = 4
+vmec_input = "../../problem/input.nfp2_QA_high_res"
+prob = qa_prob1.QAProb1(max_mode=max_mode,vmec_input = vmec_input)
 
-# find the closest point to the target
-idx = np.argmin(np.abs(aspect - aspect_target))
-x0 = X[idx]
-print('qs_mse',qs[idx],'aspect',aspect[idx])
 
-# evaluate the point
-max_mode = 5
-vmec_input = "../../problem/input.nfp4_QH_warm_start_high_res"
-prob = qh_prob1.QHProb1(max_mode=max_mode,vmec_input = vmec_input)
-raw = prob.raw(x0)
-print('qs_mse',np.mean(raw[:-1]**2),'aspect',raw[-1])
+for idx in range(len(X)):
+  """
+  Resetting the problem helps avoid failures!!!
+  """
+  prob = qa_prob1.QAProb1(max_mode=max_mode,vmec_input = vmec_input)
+  x0 = X[idx]
+  print('qs_mse',qs[idx],'aspect',aspect[idx])
+  # evaluate the point
+  # try to get rid of failures
+  prob.vmec.indata.mpol = 7
+  prob.vmec.indata.ntor = 7
+  raw = prob.raw(x0)
+  print('qs_mse',np.mean(raw[:-1]**2),'aspect',raw[-1])
+
+  # if fail: remove the wout, and evaluate again
+  if np.mean(raw[:-1]**2) == np.inf:
+    wout_files = glob.glob("wout_nfp2_QA_high_res_*.nc")
+    assert len(wout_files) == 1
+    os.remove(wout_files[0])
+    prob.vmec.indata.mpol = 8
+    prob.vmec.indata.ntor = 8
+    raw = prob.raw(x0)
+    print('qs_mse',np.mean(raw[:-1]**2),'aspect',raw[-1])
+
+  # get the wout file
+  wout_files = glob.glob("wout_nfp2_QA_high_res_*.nc")
+  assert len(wout_files) == 1
+
+  # if success: move the wout file
+  if np.mean(raw[:-1]**2) < np.inf:
+    os.rename(wout_files[0], f"points_for_matt/wout_QA_nfp2_aspect_{raw[-1]}.nc")
+  else:
+    # remove the wout file
+    os.remove(wout_files[0])
