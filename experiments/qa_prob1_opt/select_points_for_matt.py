a90a7c14e27293962e973742d07d2a8152f0ad1f
Misha Padidar <map454@cornell.edu>
2022-05-11 16:12:06 -0400

    updated script to deal with new iota indexing and removed uncessary code that prevented sim failures

--- experiments/qa_prob1_opt/select_points_for_matt.py
+++ experiments/qa_prob1_opt/select_points_for_matt.py
@@ -19,45 +19,26 @@ FX = indata['FX']
 aspect = np.copy(FX[:,0])
 qs = np.copy(FX[:,1])
 
-max_mode = 4
+max_modes = {8:1,24:2,48:3,80:4,120:5}
 vmec_input = "../../problem/input.nfp2_QA_high_res"
-prob = qa_prob1.QAProb1(max_mode=max_mode,vmec_input = vmec_input)
 
 
 for idx in range(len(X)):
-  """
-  Resetting the problem helps avoid failures!!!
-
-  If a point cannot be evaluated try using
-  mpol=ntor=6,7,8,9,10.
-  """
-  prob = qa_prob1.QAProb1(max_mode=max_mode,vmec_input = vmec_input)
   x0 = X[idx]
-  print('qs_mse',qs[idx],'aspect',aspect[idx])
+  max_mode = max_modes[len(x0)]
+  if max_mode < 5:
+    continue
+  prob = qa_prob1.QAProb1(max_mode=max_mode,vmec_input = vmec_input)
   # evaluate the point
-  # try to get rid of failures
-  prob.vmec.indata.mpol = 7
-  prob.vmec.indata.ntor = 7
   raw = prob.raw(x0)
-  print('qs_mse',np.mean(raw[:-1]**2),'aspect',raw[-1])
 
-  # if fail: remove the wout, and evaluate again
-  if np.mean(raw[:-1]**2) == np.inf:
-    wout_files = glob.glob("wout_nfp2_QA_high_res_*.nc")
-    assert len(wout_files) == 1
-    os.remove(wout_files[0])
-    prob.vmec.indata.mpol = 8
-    prob.vmec.indata.ntor = 8
-    raw = prob.raw(x0)
-    print('qs_mse',np.mean(raw[:-1]**2),'aspect',raw[-1])
+  print('max_mode',max_mode)
+  print('qs_mse',qs[idx],'aspect',aspect[idx])
+  qs_mse = np.mean(raw[:-2]**2)
+  print('qs_mse',qs_mse,'aspect',raw[-2])
+  print("")
 
   # get the wout file
   wout_files = glob.glob("wout_nfp2_QA_high_res_*.nc")
   assert len(wout_files) == 1
-
-  # if success: move the wout file
-  if np.mean(raw[:-1]**2) < np.inf:
-    os.rename(wout_files[0], f"points_for_matt/wout_QA_nfp2_aspect_{raw[-1]}.nc")
-  else:
-    # remove the wout file
-    os.remove(wout_files[0])
+  os.rename(wout_files[0], f"points_for_matt/wout_QA_nfp2_qs_mse_{qs_mse}_aspect_{raw[-2]}_max_mode_{max_mode}.nc")
