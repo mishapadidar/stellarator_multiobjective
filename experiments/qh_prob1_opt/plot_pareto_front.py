1a06d5c5b9cac8486bf7c8720c8d8fe666f80bb0
mishapadidar <map454@cornell.edu>
2023-03-14 12:04:36 -0400

    added indicators to denote efficient points

--- experiments/qh_prob1_opt/plot_pareto_front.py
+++ experiments/qh_prob1_opt/plot_pareto_front.py
@@ -12,7 +12,11 @@ from is_pareto_efficient import is_pareto_efficient
 import matplotlib
 plt.rc('font', family='serif')
 plt.rc('text.latex', preamble=r'\usepackage{amsmath,bm}')
-matplotlib.rcParams.update({'font.size': 16})
+matplotlib.rcParams.update({'font.size': 18})
+
+colors = ['#377eb8', '#ff7f00', '#4daf4a',
+          '#f781bf', '#a65628', '#984ea3',
+          '#999999', '#e41a1c', '#dede00']
 
 """
 plot QS^2 error via aspect
@@ -36,8 +40,16 @@ idx_pareto = is_pareto_efficient(FX)
 aspect_pareto = FX[idx_pareto,0]
 qs_pareto = FX[idx_pareto,1]
 
+
+# paraview points that we are plotting
+example_points = np.array([
+        [3.499999825854719,4.4651239341053535e-09],
+        [5.640672937579123,5.646371127198081e-10],
+        [8.75152077871298 ,3.074346514048328e-10]])
+
 # turn qs_mse back to qs ratio residual
 qs_pareto = 44352*qs_pareto
+example_points[:,1] *= 44352
 
 # plot
 fig,ax = plt.subplots(figsize=(8,8))
@@ -45,6 +57,9 @@ if plot_all_points:
   plt.scatter(aspect_list,qs_list,alpha=0.5)
 #plt.scatter(aspect_pareto,qs_pareto,s=30,color='k',label='pareto front',rasterized=True)
 plt.scatter(aspect_pareto,qs_pareto,s=30,color='k',rasterized=True,zorder=100)
+plt.scatter(example_points[0,0],example_points[0,1],s=160,color=colors[0],marker='*',zorder=100)
+plt.scatter(example_points[1,0],example_points[1,1],s=100,color=colors[1],marker='s',zorder=100)
+plt.scatter(example_points[2,0],example_points[2,1],s=120,color=colors[2],marker='d',zorder=100)
 plt.xlabel('Aspect Ratio')
 plt.ylabel('Quasisymmetry Ratio Residual')
 plt.yscale('log')
@@ -52,11 +67,11 @@ plt.yscale('log')
 
 # set the yticks
 plt.yticks([1e-3,3.162e-4,1e-4,3.162e-5,1e-5])
-ticks,labels= plt.yticks()
-labels[2] = ""
-plt.yticks(ticks,labels)
+#ticks,labels= plt.yticks()
+#labels[2] = ""
+#plt.yticks(ticks,labels)
 
-ax.yaxis.set_label_coords(-0.035,0.5)
+#ax.yaxis.set_label_coords(-0.035,0.5)
 
 plt.grid(linewidth=2,zorder=1)
   
